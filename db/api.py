8105fc0fb21d9f83ad6fda0e4fd8c1cc524c790f
vodves <khaziev_irekk@mail.ru>
2021-01-03 23:30:25 +0500

    2.0.0

--- db/api.py
+++ db/api.py
@@ -2,26 +2,25 @@ import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 from db.settings import TOKEN
+from db.settings import admins
 
 vk_session = vk_api.VkApi(token=TOKEN)
 vk = vk_session.get_api()
 
 
-def select_method(data, user_id):
-    body = data["object"]["message"]["text"].lower()
-    if body == "отправить":
-        send_message(user_id, "Привет")
-    else:
-        items = ("Отправить|g", "line", "Помощь|r", "Главное меню|b")
-        keyboard = generate_keyboard(items)
-        send_message(user_id, "главное меню", keyboard=keyboard)
+def admin(user_id):
+    items = (
+        "Добавить специальность|g", "Удалить специальность|r", "line", "Добавить группу|g", "Удалить группу|r", "line",
+        "Включить каждодневное напоминание|g", "Выключить каждодневное напоминание|r")
+    keyboard = generate_keyboard(items)
+    message = "Меню управления ботом: "
+    send_message(user_id, message, keyboard=keyboard)
 
 
 def main_menu():
-    keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button(f"Отправить сообщение", color=VkKeyboardColor.SECONDARY)
-    keyboard.add_line()
-    return keyboard.get_keyboard()
+    items = ("Отправить|g", "line", "Помощь|r", "Главное меню|b")
+    keyboard = generate_keyboard(items)
+    return keyboard
 
 
 def send_message(user_id, message, attachment=None, keyboard=None):
@@ -48,7 +47,7 @@ def generate_keyboard(items):
                 "b": VkKeyboardColor.PRIMARY,
                 "g": VkKeyboardColor.POSITIVE
             }
-            print(current_title, current_color)
+            # (current_title, current_color)
 
             for key, item in colors.items():
                 if key == current_color:
@@ -56,3 +55,14 @@ def generate_keyboard(items):
 
             keyboard.add_button(current_title, color=current_color)
     return keyboard.get_keyboard()
+
+
+def select_method(data, user_id):
+    body = data["object"]["message"]["text"].lower()
+    if body == "отправить":
+        send_message(user_id, "Привет")
+    elif body == "админ":
+        if user_id in admins:
+            admin(user_id)
+    else:
+        send_message(user_id, "Главное меню", keyboard=main_menu())
