5bdd3337e1803606a77c1b554fd46f135232c9d7
vodves <khaziev_irekk@mail.ru>
2021-02-07 00:06:41 +0500

    test'

--- tasks.py
+++ tasks.py
@@ -1,8 +1,10 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'db.settings')
 from apscheduler.schedulers.blocking import BlockingScheduler
 from db.api import send_checked_message_admin
 
 
 scheduler = BlockingScheduler(timezone='Asia/Yekaterinburg')
 # scheduler.add_job(send_checked_message_admin, "cron", day_of_week='mon-sat', hour=12, minute=00)
-scheduler.add_job(send_checked_message_admin, "cron", day_of_week='mon-sat', hour=23, minute=55)
+scheduler.add_job(send_checked_message_admin, "cron", hour=0, minute=20)
 scheduler.start()
